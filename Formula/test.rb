a14804d5468f4002e43e7dd3051d9179278804dc
Dokyung Lee <112409928+zipdoki@users.noreply.github.com>
2025-12-24 16:07:44 +0900

    brew test

---
+++ Formula/test.rb
@@ -0,0 +1,21 @@
+class Test < Formula
+  desc "brew test"
+  homepage "https://github.com/zipdoki/code-snippet"
+  version "0.0.1"
+  license "MIT"
+
+  ENV["CGO_ENABLED"] = "0"
+
+  on_macos do
+    url "https://github.com/zipdoki/code-snippet/releases/download/test/test.txt"
+    sha256 "f2ca1bb6c7e907d06dafe4687e579fce76b37e4e93b7605022da52e6ccc26fd2"
+  end
+
+  def install
+    bin.install "test"
+  end
+
+  test do
+    system "#{bin}/test", "--version"
+  end
+end
